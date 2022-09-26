86a67bbf8c0d54f8144c166a80fa58b264ed7aa1
Cassiano S <cassiano@scheidemantel.com.br>
2022-09-26 20:43:45 -0300

    fix: adicionado arquivos de estilizaca

--- lista-de-tarefas-app/src/index.js
+++ lista-de-tarefas-app/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Rotas from "./Rotas";
+import Rotas from "Rotas";
+import "utils/estilos/principal.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Rotas />
+      <main className="principal">
+        <Rotas />
+      </main>
     </BrowserRouter>
   </React.StrictMode>
 );
