2e208e5ab6fa5f49a040c0fa0b7aa741a813e2aa
Cassiano S <cassiano@scheidemantel.com.br>
2022-09-26 21:07:30 -0300

    fix: criando contexto da sessao e adicionado arquivos json backend

--- lista-de-tarefas-app/src/Rotas/Rotas.js
+++ lista-de-tarefas-app/src/Rotas/Rotas.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Login, CardTarefa } from "paginas";
+import { UsuarioContexto } from "contexto/Usuario";
 
 const Rotas = () => {
+  const [sessao, setSessao] = useState({ logado: true });
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="lista-de-tarefas" element={<CardTarefa />} />
-      <Route path="*" element={<Navigate to="login" replace />} />
-    </Routes>
+    <UsuarioContexto.Provider value={[sessao, setSessao]}>
+      <Routes>
+        {sessao && sessao.logado ? (
+          <>
+            <Route path="lista-de-tarefas" element={<CardTarefa />} />
+            <Route
+              path="*"
+              element={<Navigate to="lista-de-tarefas" replace />}
+            />
+          </>
+        ) : (
+          <>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<Navigate to="login" replace />} />
+          </>
+        )}
+      </Routes>
+    </UsuarioContexto.Provider>
   );
 };
 
